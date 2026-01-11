1021652c0951d937449a05b7cea41d0b9e9c7a19
karimzade_k <karimzade_k@STHQ0118-01>
2026-01-11 19:58:44 +0400

    Add project files.

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,33 @@
+﻿//// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+
+
+
+using System.Net;
+using System.Text;
+
+#region Comeback
+
+// HttpListener httpListener = new HttpListener();
+   
+   
+// httpListener.Prefixes.Add("http://localhost:27001/");
+   
+// httpListener.Start();
+   
+// Console.WriteLine("Listening ....");
+   
+// while (true)
+// {
+//     var context = httpListener.GetContext();
+   
+//     context.Response.OutputStream.Write(Encoding.UTF8.GetBytes("Hello world!"));
+//     context.Response.Close();
+   
+//     Console.WriteLine("Response return...");
+// }
+
+#endregion
+
+
