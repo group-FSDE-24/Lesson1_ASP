20e3fa08a8070dc149a4bc3f24053742fadae8d5
Karimzada Kamran Natiq <karimzade_k@itstep.edu.az>
2026-01-11 20:43:48 +0400

    webhost giris

---
+++ MyFirstApp/Helper/WebHost.cs
@@ -0,0 +1,95 @@
+﻿using System.Net;
+
+namespace MyFirstApp.Helper;
+
+public class WebHost
+{
+    private short _port { get; set; }
+    private HttpListener _httpListener { get; set; }
+
+
+    public WebHost(short port)
+    {
+        _port = port;
+    }
+
+
+    public void Run()
+    {
+        _httpListener = new HttpListener();
+
+        _httpListener.Prefixes.Add($"http://localhost:{_port}/");
+
+        _httpListener.Start();
+
+        Console.WriteLine($"Httplistener started on {_port}");
+
+        while (true)
+        {
+            var context = _httpListener.GetContext();
+
+            Task.Run(() =>
+            {
+                HandlerRequest(context);
+            });
+
+        }
+
+
+    }
+
+    private void HandlerRequest(HttpListenerContext context)
+    {
+        var request = context.Request;
+        var response = context.Response;
+
+        // Rawurl -> /contact
+        // http://localhost:27001/contact.html
+
+        var str = request.RawUrl;
+
+        var requestPath = str.Substring(1); // contact
+
+        try
+        {
+            if (!Path.HasExtension(requestPath))
+                requestPath += ".html";
+
+
+            var fileName = "../../../Views/" + requestPath;
+
+            if(File.Exists(fileName))
+            {
+                response.StatusCode = 200;
+
+
+
+
+
+
+            }
+            else
+            {
+
+            }
+
+
+
+        }
+        catch (Exception ex)
+        {
+
+        }
+        finally
+        {
+
+        }
+
+
+
+
+
+    }
+
+
+}
--- MyFirstApp/Program.cs
+++ MyFirstApp/Program.cs
@@ -4,35 +4,37 @@
 
 
 
+using MyFirstApp.Helper;
 using System.Net;
 using System.Text;
 
 #region Comeback
 
 // HttpListener httpListener = new HttpListener();
-   
-   
+
+
 // httpListener.Prefixes.Add("http://localhost:27001/");
-   
+
 // httpListener.Start();
-   
+
 // Console.WriteLine("Listening ....");
-   
+
 // while (true)
 // {
 //     var context = httpListener.GetContext();
-   
+
 //     context.Response.OutputStream.Write(Encoding.UTF8.GetBytes("Hello world!"));
 //     context.Response.Close();
-   
+
 //     Console.WriteLine("Response return...");
 // }
 
 #endregion
 
 
+var webhost = new WebHost(27001);
 
-
+webhost.Run();
 
 
 
